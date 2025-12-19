13fc9e5b743265f430d585c065500c63ffa3d95c
Igor Bastos Alencar <alencar29htcss@gmail.com>
2025-12-19 07:24:51 -0300

    feat: created project

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useRef, useState } from "react";
+import SpinWheel, { SpinWheelHandle, Sector } from "ts-spin-wheel";
+import IBoiaHouse from "./assets/logo_casa.jpg";
+import { wheelData } from "./data/data";
+import { Modal } from "./components/modal";
+import Confetti from "react-confetti";
+import Aud from "./assets/audio.mp3";
+import "./style.scss";
+// ... (mantenha os imports e o wheelData)
+
+const App = () => {
+  const [dataWheel, setDataWheel] = useState(wheelData);
+  const [party, setParty] = useState(false);
+  const wheelRef = useRef<SpinWheelHandle>(null);
+  const audioRef = useRef<any | null>(null);
+
+  const [windowDimension, setWindowDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const triggerSpin = () => {
+    if (wheelRef.current) {
+      wheelRef.current.spin();
+      audioRef.current.play();
+      audioRef.current.currentTime = 4;
+    }
+  };
+
+  const detectSize = () => {
+    setWindowDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", detectSize);
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
+  }, []);
+
+  const removedPerson = (label, onClose) => {
+    const newDataWheel = dataWheel.slice();
+
+    const removedItem = newDataWheel.filter((item) => {
+      return item.label !== label ? item : null;
+    });
+
+    setDataWheel(removedItem);
+    onClose();
+  };
+
+  return (
+    <div className="wheel-container">
+      {party && (
+        <Confetti
+          width={windowDimension.width}
+          height={windowDimension.height}
+          recycle={false}
+          numberOfPieces={1000}
+          gravity={0.2}
+        />
+      )}
+      <div className="wheel-stage">
+        <div className="side-indicator" onClick={() => triggerSpin()}></div>
+
+        <div className="wheel-wrapper">
+          <div className="center-logo-overlay" onClick={() => triggerSpin()}>
+            <img src={IBoiaHouse} alt="Logo casa da boia" />
+          </div>
+
+          <SpinWheel
+            ref={wheelRef}
+            sectors={dataWheel}
+            size={800}
+            onSpinEnd={() => setParty(true)}
+            labelFontSize={24}
+            labelOrientation="vertical"
+            spinButtonText=""
+            enableSound={false}
+            spinButtonStyles={{ display: "none" }}
+            customModalContent={(winner, onClose) => (
+              <Modal
+                label={winner.label}
+                onClose={() => {
+                  setParty(false);
+                  onClose();
+                }}
+                onRemoved={removedPerson}
+              />
+            )}
+          />
+          <audio ref={audioRef} src={Aud} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data/data.ts
@@ -0,0 +1,48 @@
+import type { Sector } from "ts-spin-wheel";
+
+export const wheelData: Sector[] = [
+  { label: "Adriana R.", color: "#9E714C", text: "#fff" },
+  { label: "Anderson", color: "#fca44cc9", text: "#000" },
+  { label: "Antonio", color: "#e3bd97ff", text: "#000" },
+  { label: "Daniel", color: "#855d37a7", text: "#fff" },
+  { label: "Denilson", color: "#D7AC81", text: "#000" },
+  { label: "Diego", color: "#ffffffce", text: "#000" },
+  { label: "Fernando", color: "#fbe9d5", text: "#000" },
+
+  { label: "Flavia", color: "#9E714C", text: "#fff" },
+  { label: "Giovanna", color: "#fca44cc9", text: "#000" },
+  { label: "Hugo", color: "#E3BD97", text: "#000" },
+  { label: "Janaina", color: "#855d37a7", text: "#fff" },
+  { label: "Jhoni", color: "#D7AC81", text: "#000" },
+  { label: "Joao", color: "#ffffffce", text: "#000" },
+  { label: "Johnatas", color: "#fbe9d5", text: "#000" },
+
+  { label: "Guilherme", color: "#fca44cc9", text: "#000" },
+  { label: "Katia", color: "#E3BD97", text: "#000" },
+  { label: "Leila", color: "#D7AC81", text: "#000" },
+  { label: "Leonardo", color: "#ffffffce", text: "#000" },
+  { label: "Manuel", color: "#fbe9d5", text: "#000" },
+
+  { label: "Marcia", color: "#9E714C", text: "#fff" },
+  { label: "Marco", color: "#fca44cc9", text: "#000" },
+  { label: "Marcos", color: "#E3BD97", text: "#000" },
+  { label: "Michael", color: "#855d37a7", text: "#fff" },
+  { label: "Miguel", color: "#D7AC81", text: "#000" },
+  { label: "Neuvanir", color: "#ffffffce", text: "#000" },
+  { label: "Niverson", color: "#fbe9d5", text: "#000" },
+
+  { label: "Patricia Q.", color: "#9E714C", text: "#fff" },
+  { label: "Ricardo A.", color: "#fca44cc9", text: "#000" },
+  { label: "Ricardo G.", color: "#E3BD97", text: "#000" },
+  { label: "Roberto", color: "#855d37a7", text: "#fff" },
+  { label: "Rosana", color: "#D7AC81", text: "#000" },
+  { label: "Sulivan", color: "#ffffffce", text: "#000" },
+  { label: "Tiago", color: "#fbe9d5", text: "#000" },
+
+  { label: "Vitor", color: "#9E714C", text: "#fff" },
+  { label: "Wigor", color: "#fca44cc9", text: "#000" },
+  { label: "Maria", color: "#855d37a7", text: "#fff" },
+  { label: "Adriana M.", color: "#D7AC81", text: "#000" },
+  { label: "Monique", color: "#ffffffce", text: "#000" },
+  { label: "Patricia B.", color: "#fbe9d5", text: "#000" },
+];
